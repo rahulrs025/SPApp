34d69e7b0fff961f0140d42d6b1fd556764bdb6c
resemblesystems-git <50731694+resemblesystems-git@users.noreply.github.com>
2020-05-02 02:50:58 +0530

    Updates

---
+++ precache-manifest.89c7b34d9d9e2a5b1228ae8144166797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6545e1aeb2bdaed972bc7d044737c9",
+    "url": "/SPApp/index.html"
+  },
+  {
+    "revision": "972b5f80620028e9a2d4",
+    "url": "/SPApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0e32a03cd3c366a2c5e3",
+    "url": "/SPApp/static/js/2.9bc27cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SPApp/static/js/2.9bc27cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972b5f80620028e9a2d4",
+    "url": "/SPApp/static/js/main.1c5399e8.chunk.js"
+  },
+  {
+    "revision": "3b5b01634da26df219cb",
+    "url": "/SPApp/static/js/runtime-main.ce9dbbb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SPApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
